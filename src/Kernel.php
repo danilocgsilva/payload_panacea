6bfbd62c1ff1fd51a25389e96b752c843ea21756
Danilo Silva <contact@danilocgsilva.me>
2024-11-06 00:25:58 +0000

    Namespace changed

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Danilocgsilva\PayloadPanacea;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
